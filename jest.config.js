624bbc98f0cc1b86acde25d518b9efd53fa0c01d
kk <342317003@qq.com>
2019-04-01 10:30:25 +0800

    build: 修改package.json依赖

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,6 @@ module.exports = {
     preset: 'react-native',
     transform: {
         '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-        '\\.(ts|tsx)$': 'ts-jest',
-    },
-    globals: {
-        'ts-jest': {
-            tsConfig: 'tsconfig.jest.json',
-        },
     },
     moduleFileExtensions: ['ts', 'tsx', 'js'],
     testMatch: ['**/__tests__/**/*.(js|ts|tsx)', '**/?(*.)+(spec|test).(js|ts|tsx)'],
